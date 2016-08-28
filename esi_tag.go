9ca5adc8eb21f8d73430563d71ba0e36b4ba613c
Cyrill Schumacher <cyrill@schumacher.fm>
2016-08-28 20:42:38 +0200

    Implement stream based ESI tag parser; still buggy

--- esi_tag.go
+++ esi_tag.go
@@ -15,6 +15,8 @@ type Conditioner interface { // no, not the shampoo ;-)
 
 type ESITag struct {
 	RawTag         []byte
+	TagStart       int // byte slice index position in the whole slice
+	TagEnd         int // byte slice index position in the whole slice
 	Sources        []fmt.Stringer
 	Key            []byte
 	TTL            time.Duration
@@ -28,21 +30,22 @@ type ESITag struct {
 type ESITags []*ESITag
 
 var (
-	esiTagStart    = []byte(`<esi:include`)
-	esiTagStartLen = len(esiTagStart)
-	esiTagEnd      = []byte(`/>`)
+	esiTagStart = []byte(`<esi:include`)
+	esiTagEnd   = []byte(`/>`)
 )
 
 // ParseESITags parses a stream of HTML data to look for ESI Tags
-func ParseESITags(r io.Reader) (ret ESITags,_ error) {
-	const innerBufSize = 64
-	const outerBufSize = innerBufSize * 4
+func ParseESITags(r io.Reader) (ret ESITags, _ error) {
+	const innerBufSize = 64 // min size len(esiTagStart)
 
 	br := bufio.NewReader(r)
 
 	buffer := make([]byte, innerBufSize)
 	var globalPos int
 	var startPos = -1
+	var endPos = -1
+	var tagIndex = -1
+	var totalTagEnds int
 	for {
 		n, err := br.Read(buffer)
 		if err == io.EOF {
@@ -55,20 +58,9 @@ func ParseESITags(r io.Reader) (ret ESITags,_ error) {
 		data := buffer[:n]
 
 		if startPos < 0 {
-			startPos = bytes.Index(data, esiTagStart)
-			if startPos < 0 {
-				// look ahead without advancing the reader
-				peek, err := br.Peek(esiTagStartLen)
-				if err == nil {
-					data = append(data, peek...)
-					startPos = bytes.Index(data, esiTagStart)
-					if startPos > -1 { // yay found via look ahead! so advance the reader
-						if _,err := br.Discard(esiTagStartLen); err != nil {
-							panic(err) // todo
-						}
-					}
-				}
-			}
+			// find start position and do a look ahead. if look ahead, then
+			// return the new data slice and forward bufio.Reader
+			startPos, data = getPosition(br, data, esiTagStart)
 
 			fmt.Printf("Start: globalPos %03d startPos %04d DATA: %q\n", globalPos, startPos, data)
 
@@ -76,25 +68,86 @@ func ParseESITags(r io.Reader) (ret ESITags,_ error) {
 				clearBuffer(buffer)
 				continue
 			}
+			ret = append(ret, &ESITag{
+				RawTag:   make([]byte, 0, 256),
+				TagStart: globalPos,
+			})
+			tagIndex++
+			data = data[startPos:] // discard any data before tag start because it might contain "/>"
+			globalPos += len(esiTagStart)
 		}
 
-		endPos := bytes.Index(data[startPos:], esiTagEnd)
-		// todo look adhead
+		endPosFound := false
+		if startPos > -1 { // we know we have found a starting tag
+			var ep int
+			// we do not know if the end tag already is in the data, even with a look ahead.
+			ep, data = getPosition(br, data, esiTagEnd)
+			dataLen := len(data)
 
-		fmt.Printf("End: globalPos %03d startPos %04d endPos %04d DATA: %q\n", globalPos, startPos,endPos, data)
+			fmt.Printf("startPos %d | ep %d | newdata: %q\n", startPos, ep, data)
 
+			if ep > -1 {
+				ret[tagIndex].RawTag = append(ret[tagIndex].RawTag, data[len(esiTagEnd):]...)
+				endPosFound = true
+				if endPos < 0 {
+					endPos = 0
+				}
+				endPos += ep + 1
+			} else {
+				// as long as we don't have found the end tag, append the data to the RawTag
+				ret[tagIndex].RawTag = append(ret[tagIndex].RawTag, data...)
+				endPos += dataLen
+			}
+		}
 
-		if endPos < 0 {
+		//if endPos < 0 {
+		if !endPosFound {
 			clearBuffer(buffer)
 			continue
 		}
-
-
-
-		println("2: ", string(data), startPos, endPos)
+		if startPos > -1 && endPosFound {
+			//fmt.Printf("End: globalPos %03d startPos %04d endPos %04d DATA: %q\n", globalPos, startPos, endPos, ret[tagIndex].RawTag)
+			// trim the RawTag buffer until the EndTag
+			ret[tagIndex].RawTag = ret[tagIndex].RawTag[:endPos]
+			ret[tagIndex].TagEnd = globalPos
+			startPos = -1
+			endPos = -1
+			endPosFound = false
+			clearBuffer(buffer)
+			totalTagEnds++
+		}
 	}
 
-	return nil, nil
+	if have, want := totalTagEnds, len(ret); have != want {
+		// human error message, to make clear where the bug is.
+		buf := new(bytes.Buffer)
+		for _, t := range ret {
+			fmt.Fprintf(buf, "%q", t.RawTag)
+			buf.WriteRune('\n')
+		}
+		return nil, fmt.Errorf("[caddyesi] Opening close tag mismatch!\n%s", buf)
+	}
+	return ret, nil
+}
+
+// getPosition searches a sep within data
+func getPosition(br *bufio.Reader, data, sep []byte) (startPos int, _ []byte) {
+	sepLen := len(sep)
+	startPos = bytes.Index(data, sep)
+	if startPos < 0 {
+		// look ahead without advancing the reader
+		peek, err := br.Peek(sepLen)
+		if err == nil {
+			data = append(data, peek...)
+			startPos = bytes.Index(data, sep)
+			if startPos > -1 { // yay found via look ahead! so advance the reader
+				if _, err := br.Discard(sepLen); err != nil {
+					panic(err) // todo remove panic
+				}
+			}
+		}
+	}
+	return startPos, data
 }
 
 func clearBuffer(buffer []byte) {
--- esi_tag_test.go
+++ esi_tag_test.go
@@ -1,45 +1,99 @@
 package esi
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseESITags(t *testing.T) {
+func mustOpenFile(file string) io.ReadCloser {
+	f, err := os.Open("testdata/" + file)
+	if err != nil {
+		panic(fmt.Sprintf("%s => %s", file, err))
+	}
+	return f
+}
 
-	var testRunner = func(file string, wantTags ESITags, wantErr string) func(*testing.T) {
-		return func(t *testing.T) {
-			f, err := os.Open("testdata/" + file)
-			if err != nil {
-				t.Fatalf("%s => %s", file, err)
-			}
-			defer f.Close()
+func strReader(s string) io.ReadCloser {
+	return ioutil.NopCloser(strings.NewReader(s))
+}
 
-			haveTags, err := ParseESITags(f)
-			if wantErr != "" {
-				assert.Nil(t, haveTags)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), wantErr)
-				return
-			}
-			assert.NoError(t, err)
-			if have, want := len(haveTags), len(wantTags); have != want {
-				t.Fatalf("ESITags Count does not match: Have: %v Want: %v; File: %q", have, want, file)
-			}
-			for i, tg := range wantTags {
-				assert.Exactly(t, string(tg.RawTag), string(haveTags[i].RawTag))
-			}
+var testRunner = func(rc io.ReadCloser, wantTags ESITags, wantErr string) func(*testing.T) {
+	return func(t *testing.T) {
+		defer rc.Close()
+		haveTags, err := ParseESITags(rc)
+		if wantErr != "" {
+			assert.Nil(t, haveTags)
+			assert.Error(t, err)
+			assert.Contains(t, err.Error(), wantErr)
+			return
+		}
+		assert.NoError(t, err)
+		if have, want := len(haveTags), len(wantTags); have != want {
+			t.Fatalf("ESITags Count does not match: Have: %v Want: %v", have, want)
+		}
+		for i, tg := range wantTags {
+			assert.Exactly(t, string(tg.RawTag), string(haveTags[i].RawTag))
 		}
 	}
-	t.Run("Page1", testRunner(
-		"page0.html",
+}
+
+func TestParseESITags_File(t *testing.T) {
+	t.Run("Page0", testRunner(
+		mustOpenFile("page0.html"),
 		ESITags{
 			&ESITag{
 				RawTag: []byte("<esi:include   src=\"https://micro.service/esi/foo\"\n                                            />"),
 			},
 		},
 		"",
+	))
+	t.Run("Page1", testRunner(
+		mustOpenFile("page1.html"),
+		ESITags{
+			&ESITag{
+				RawTag: []byte("<esi:include src=\"https://micro.service/esi/foo\" timeout=\"8ms\" onerror=\"mylocalFile.html\"/>"),
+			},
+		},
+		"",
+	))
+	t.Run("Page2", testRunner(
+		mustOpenFile("page2.html"),
+		ESITags{
+			&ESITag{
+				RawTag: []byte("<esi:include src=\"https://micro.service/customer/account\" timeout=\"8ms\" onerror=\"accountNotAvailable.html\"/>"),
+			},
+			&ESITag{
+				RawTag: []byte("<esi:include srsrc=\"https://micro.service/checkout/cat\" timeout=\"9ms\" onerror=\"nonocart.html\" forwardheaders=\"Cookie,Accept-Language,Authorization\"/>"),
+			},
+		},
+		"",
+	))
+}
+
+func TestParseESITags_String(t *testing.T) {
+	t.Run("Line_80", testRunner(
+		strReader(``),
+		nil,
+		"",
+	))
+	t.Run("Line_85", testRunner(
+		strReader("x \x00 <i>x</i>          <esi:include\x00 src=\"https:...\" />\x00"),
+		ESITags{
+			&ESITag{
+				RawTag: []byte("<esi:include\x00\x00 src=\"https:...\" />"),
+			},
+		},
+		"",
+	))
+	t.Run("Line_73", testRunner(
+		strReader(`<esi:include src="..." <b>`),
+		nil,
+		"[caddyesi] Opening close tag mismatch!\n\"<esi:include src=\\\"...\\\" <b>\"\n",
 	))
 }
