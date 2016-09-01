fc527a214789b8e5baeb70aab3ad0122ac9213e3
Cyrill Schumacher <cyrill@schumacher.fm>
2016-09-01 07:54:13 +0200

    Add TestESITag_ParseRaw to parse a single ESI Tag

--- esi_tag.go
+++ esi_tag.go
@@ -6,6 +6,12 @@ import (
 	"time"
 )
 
+type source string
+
+func (u source) String() string {
+	return u
+}
+
 type Conditioner interface { // no, not the shampoo ;-)
 	OK(r *http.Request) bool
 }
@@ -13,16 +19,28 @@ type Conditioner interface { // no, not the shampoo ;-)
 // ESITag represents a single ESI tag
 type ESITag struct {
 	RawTag         []byte
-	TagStart       int // byte slice index position in the whole slice
-	TagEnd         int // byte slice index position in the whole slice
-	Sources        []fmt.Stringer
-	Key            []byte
+	TagStart       int            // start position in the stream
+	TagEnd         int            // end position in the stream
+	Sources        []fmt.Stringer // creates an URL to fetch data from
+	Key            []byte         // use for lookup in key/value storages to fetch data from
 	TTL            time.Duration
 	Timeout        time.Duration
 	OnError        string
 	ForwardHeaders []string
 	ReturnHeaders  []string
 	Conditioner
+}
+
+// ParseRaw parses the RawTag field and fills the remaining fields of the
+// struct.
+func (et *ESITag) ParseRaw() error {
+	if len(et.RawTag) == 0 {
+		return nil
+	}
+
+	// r := bytes.NewReader(et.RawTag)
+
+	return nil
 }
 
 // ESITags represents a list of ESI tags
--- esi_tag_test.go
+++ esi_tag_test.go
@@ -1,1 +1,46 @@
 package esi
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestESITag_ParseRaw(t *testing.T) {
+	tests := []struct {
+		raw     []byte
+		wantErr string
+		wantTag *ESITag
+	}{
+		{
+			[]byte(`include src="https://micro.service/checkout/cart" timeout="9ms" onerror="nocart.html" forwardheaders="Cookie,Accept-Language,Authorization"`),
+			"",
+			&ESITag{
+				Sources: []fmt.Stringer{
+					source("https://micro.service/checkout/cart"),
+				},
+				Timeout:        time.Millisecond * 9,
+				OnError:        "nocart.html",
+				ForwardHeaders: []string{"Cookie", "Accept-Language", "Authorization"},
+			},
+		},
+	}
+	for i, test := range tests {
+		test.wantTag.RawTag = test.raw
+		haveET := &ESITag{
+			RawTag:   test.raw,
+			TagStart: 0,
+			TagEnd:   len(test.raw),
+		}
+		haveErr := haveET.ParseRaw()
+		if test.wantErr != "" {
+			assert.Error(t, haveErr, "Index %d", i)
+			assert.Contains(t, haveErr.Error(), test.wantErr, "Index %d", i)
+			continue
+		}
+		assert.NoError(t, haveErr)
+		assert.Exactly(t, *test.wantTag, *haveET, "Index %d", i)
+	}
+}
