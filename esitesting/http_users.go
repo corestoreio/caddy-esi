93530ccb8d23ac83635636104bb11ba263549ea0
Cyrill Schumacher <cyrill@schumacher.fm>
2016-12-31 12:58:45 +0100

    esitesting: Add parallel user testing for finding races

---
+++ esitesting/http_users.go
@@ -0,0 +1,144 @@
+// Copyright 2016-2017, Cyrill @ Schumacher.fm and the CaddyESI Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package esitesting
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
+
+// HTTPParallelUsers allows to run parallel and concurrent calls to a given
+// http.Handler.
+type HTTPParallelUsers struct {
+	// Users or also known as number of threads
+	Users int
+	// Loops each user runs these loops
+	Loops int
+	// RampUpPeriod time to take to generate to the full request force. The
+	// duration calculates: RampUpPeriod * Interval
+	RampUpPeriod int
+	// Interval an enum set of time.Nanosecond, time.Microsecond, time.Millisecond,
+	// time.Second, time.Minute, time.Hour.
+	Interval time.Duration
+	// AssertResponse provides the possibility to check the written data after each
+	// request.
+	AssertResponse func(_ *httptest.ResponseRecorder, code int, serveErr error)
+}
+
+// Header* got set within an user iteration to allow you to identify a request.
+const (
+	HeaderUserID = "X-Test-User-ID"
+	HeaderLoopID = "X-Test-Loop-ID"
+	HeaderSleep  = "X-Test-Sleep"
+)
+
+// NewHTTPParallelUsers initializes a new request producer. Users means the
+// total amount of parallel users. Each user can execute a specific loopsPerUser
+// count. The rampUpPeriod defines the total runtime of the test and the period
+// it takes to produce the finally total amount of parallel requests. The
+// interval applies to the exported constants of the time package:
+// time.Nanosecond, time.Microsecond, time.Millisecond, time.Second,
+// time.Minute, time.Hour. The total runtime calculates rampUpPeriod * interval.
+// Every (rampUpPeriod / users) a new user starts with its requests. Each user
+// request sleeps a specific equal time until the test ends. With the last
+// started user the maximum amount of parallel requests will be reached.
+func NewHTTPParallelUsers(users, loopsPerUser, rampUpPeriod int, interval time.Duration) HTTPParallelUsers {
+	switch interval {
+	case time.Nanosecond, time.Microsecond, time.Millisecond, time.Second, time.Minute, time.Hour:
+	// ok
+	default:
+		panic(fmt.Sprintf("Unknown interval %s. Only allowed time.Nanosecond, time.Microsecond, etc", interval))
+	}
+
+	return HTTPParallelUsers{
+		Users:        users,
+		Loops:        loopsPerUser,
+		RampUpPeriod: rampUpPeriod,
+		Interval:     interval,
+	}
+}
+
+func (hpu HTTPParallelUsers) sleepPerServeHTTP(userID int) time.Duration {
+	d := (float64(hpu.RampUpPeriod) / float64(userID) / float64(hpu.Loops)) * float64(hpu.Interval)
+	return time.Duration(d)
+}
+
+// serve runs the benchmark. r or rf can be nil, but not both.
+func (hpu HTTPParallelUsers) serve(rf func() *http.Request, h httpserver.Handler) {
+
+	var user = func(wg *sync.WaitGroup, userID int) {
+		for i := 1; i <= hpu.Loops; i++ {
+			sl := hpu.sleepPerServeHTTP(userID)
+			// go func(sl time.Duration) { // if go, then add WaitGroup
+			w := httptest.NewRecorder()
+			w.Header().Set(HeaderUserID, strconv.Itoa(userID))
+			w.Header().Set(HeaderLoopID, strconv.Itoa(i))
+			w.Header().Set(HeaderSleep, sl.String())
+
+			code, serveErr := h.ServeHTTP(w, rf())
+			if hpu.AssertResponse != nil {
+				hpu.AssertResponse(w, code, serveErr)
+			}
+			// }(sl)
+			time.Sleep(sl)
+		}
+		wg.Done()
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(hpu.Users)
+	var startDelay = hpu.RampUpPeriod / hpu.Users
+	var delay = new(int32)
+	for j := 1; j <= hpu.Users; j++ {
+		go func(userID int) {
+			if startDelay > 0 && userID > 1 {
+				atomic.AddInt32(delay, int32(startDelay))
+				cd := atomic.LoadInt32(delay)
+				if int(cd) <= hpu.RampUpPeriod {
+					time.Sleep(time.Duration(cd) * hpu.Interval)
+				}
+			}
+			go user(&wg, userID)
+		}(j)
+	}
+	wg.Wait()
+}
+
+// ServeHTTP starts the testing and the request gets called with http.Handler.
+// You might run into a race condition when trying to add a request body (an
+// io.ReadCloser), because multiple reads and writes into the buffer. Use the
+// function ServeHTTPNewRequest() if you need for each call to http.Handler a
+// new request object.
+func (hpu HTTPParallelUsers) ServeHTTP(r *http.Request, h httpserver.Handler) {
+	// should be refactored but for now quite ok
+	// 10 threads, 20 seconds ramp-up - start with 1 user, each 2 seconds 1 user added
+	hpu.serve(func() *http.Request {
+		return r
+	}, h)
+}
+
+// ServeHTTPNewRequest same as ServeHTTP() but creates for each iteration a new
+// fresh request which will be passed to http.Handler. Does not trigger a race
+// condition.
+func (hpu HTTPParallelUsers) ServeHTTPNewRequest(rf func() *http.Request, h httpserver.Handler) {
+	hpu.serve(rf, h)
+}
---
+++ esitesting/http_users_test.go
@@ -0,0 +1,166 @@
+// Copyright 2016-2017, Cyrill @ Schumacher.fm and the CaddyESI Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package esitesting_test
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/SchumacherFM/caddyesi/esitesting"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPParallelUsers_WrongInterval(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if r := recover(); r != nil {
+			if s, ok := r.(string); ok {
+				if have, want := s, "Unknown interval 2s. Only allowed time.Nanosecond, time.Microsecond, etc"; have != want {
+					t.Errorf("Have: %v Want: %v", have, want)
+				}
+
+			} else {
+				t.Fatalf("Expecting a string in the panic; Got: %#v", r)
+			}
+		} else {
+			t.Fatal("Expecting a panic")
+		}
+	}()
+	_ = esitesting.NewHTTPParallelUsers(1, 1, 1, time.Second*2)
+}
+
+func TestHTTPParallelUsers_Single(t *testing.T) {
+	t.Parallel()
+	tg := esitesting.NewHTTPParallelUsers(1, 1, 1, time.Nanosecond)
+	req := httptest.NewRequest("GET", "http://corestore.io", nil)
+
+	var reqCount int
+	tg.ServeHTTP(req, httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
+		// no race here because one single iteration
+		reqCount++
+		return http.StatusOK, nil
+	}))
+	if have, want := reqCount, 1; have != want {
+		t.Errorf("Request count mismatch! Have: %v Want: %v", have, want)
+	}
+}
+
+func TestHTTPParallelUsers_Long(t *testing.T) {
+	t.Parallel()
+	startTime := time.Now()
+	const (
+		users        = 4
+		loops        = 10
+		rampUpPeriod = 2
+	)
+	tg := esitesting.NewHTTPParallelUsers(users, loops, rampUpPeriod, time.Second)
+	req := httptest.NewRequest("GET", "http://corestore.io", nil)
+
+	tg.AssertResponse = func(rec *httptest.ResponseRecorder, code int, err error) {
+		assert.NoError(t, err, "%+v", err)
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderUserID))
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderLoopID))
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderSleep))
+	}
+
+	var reqCount = new(int32)
+	tg.ServeHTTP(req, httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
+		//t.Logf("UserID %s LoopID %s Sleeping %s",
+		//	rec.Header().Get(esitesting.HeaderUserID),
+		//	rec.Header().Get(esitesting.HeaderLoopID),
+		//	rec.Header().Get(esitesting.HeaderSleep),
+		//)
+		atomic.AddInt32(reqCount, 1)
+		return http.StatusOK, nil
+	}))
+
+	//t.Logf("Users %d Loops %d, RampUp %d", users, loops, rampUpPeriod)
+
+	if have, want := *reqCount, int32(users*loops); have != want {
+		t.Errorf("Request count mismatch! Have: %v Want: %v", have, want)
+	}
+
+	if have, want := int(time.Since(startTime).Seconds()), rampUpPeriod; have != want {
+		t.Errorf("Test Running Time is weird! Have: %v Want: %v", have, want)
+	}
+}
+
+func TestHTTPParallelUsers_ServeHTTPNewRequest(t *testing.T) {
+	t.Parallel()
+	startTime := time.Now()
+	const (
+		users        = 4
+		loops        = 10
+		rampUpPeriod = 2
+	)
+	tg := esitesting.NewHTTPParallelUsers(users, loops, rampUpPeriod, time.Second)
+
+	tg.AssertResponse = func(rec *httptest.ResponseRecorder, code int, err error) {
+		assert.NoError(t, err, "%+v", err)
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderUserID))
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderLoopID))
+		assert.NotEmpty(t, rec.Header().Get(esitesting.HeaderSleep))
+	}
+
+	var reqCount = new(int32)
+	// if you now use ServeHTTP() the below HanderFunc code will trigger a race condition
+	tg.ServeHTTPNewRequest(func() *http.Request {
+		return httptest.NewRequest("POST", "http://corestore.io", strings.NewReader(`#golang proverb: A little copying is better than a little dependency.`))
+	}, httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
+		// read the body of the post request
+		buf := make([]byte, 16)
+		defer func() {
+			if err := r.Body.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		for {
+			n, err := r.Body.Read(buf)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+			buf = buf[:n]
+			if s := hex.EncodeToString(buf); len(s) < 4 {
+				// just do at least something ...
+				// t.Fatal won't work here to effectively terminate the test-goroutine.
+				panic(fmt.Sprintf("HEX too short: %q with buf %q", s, buf))
+			}
+		}
+
+		atomic.AddInt32(reqCount, 1)
+		return http.StatusOK, nil
+	}))
+
+	//t.Logf("Users %d Loops %d, RampUp %d", users, loops, rampUpPeriod)
+
+	if have, want := *reqCount, int32(users*loops); have != want {
+		t.Errorf("Request count mismatch! Have: %v Want: %v", have, want)
+	}
+
+	if have, want := int(time.Since(startTime).Seconds()), rampUpPeriod; have != want {
+		t.Errorf("Test Running Time is weird! Have: %v Want: %v", have, want)
+	}
+}
