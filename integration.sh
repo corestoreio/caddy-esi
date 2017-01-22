6607d17d3d2e0d23790ff41e7074fb3b9e692d4b
Cyrill Schumacher <cyrill@schumacher.fm>
2017-01-22 10:23:29 +0100

    travis: Add redis/memcache; remove ht.bin and use go run
    Seems like someting is caching ...

--- integration.sh
+++ integration.sh
@@ -6,14 +6,16 @@ _ "github.com/SchumacherFM/caddyesi"\'$'\n' $GOPATH/src/github.com/mholt/caddy/c
 sed -i.bak '/directives that add middleware to the stack/a\
 "esi",\'$'\n' $GOPATH/src/github.com/mholt/caddy/caddyhttp/httpserver/plugin.go
 
+redis-cli SET "product_001" "Catalog Product 001"
+redis-cli SET "category_tree" "Catalog Category Tree"
+
 go build -o caddy.bin $GOPATH/src/github.com/mholt/caddy/caddy/main.go
-go build -o ht.bin $GOPATH/src/github.com/SchumacherFM/caddyesi/ht/*.go
 
 nohup ./caddy.bin -conf ./Caddyfile &
 sleep 5
-./ht.bin
+go run $GOPATH/src/github.com/SchumacherFM/caddyesi/ht/*.go
 
-# temporary debug
+echo "temporary debugging cURL requests"
 curl -s 'http://127.0.0.1:2017/page02.html' | wc
 curl -i 'http://127.0.0.1:2017/page02.html'
 
