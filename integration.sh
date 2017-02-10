343c3e436f10ff418fbff8d4ba83c8ebaa15d16a
Cyrill Schumacher <cyrill@schumacher.fm>
2017-02-10 09:17:15 +0100

    Travis: more verbose errors, exclude .pb.go files in golint

--- integration.sh
+++ integration.sh
@@ -15,10 +15,9 @@ redis-cli -n 1 SET "checkout_cart" "You have 10 items in your cart"
 # redis-cli -n 1 GET "checkout_cart"
 
 go build -tags esiall -race -o caddy.bin $GOPATH/src/github.com/mholt/caddy/caddy/main.go
-# go run $GOPATH/src/github.com/mholt/caddy/caddy/main.go -conf ./Caddyfile
+# go run -tags esiall $GOPATH/src/github.com/mholt/caddy/caddy/main.go -conf ./Caddyfile
 
-./caddy.bin -conf ./Caddyfile &> caddy_stdout.log && cat caddy_stdout.log &
+./caddy.bin -conf ./Caddyfile &
 sleep 6
 go run $GOPATH/src/github.com/SchumacherFM/caddyesi/ht/*.go
-echo "catting caddy_stdout.log"
-cat caddy_stdout.log
+
