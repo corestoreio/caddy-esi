85568afcaea5171abd763eeaf7ec41157598b049
Cyrill Schumacher <cyrill@schumacher.fm>
2017-02-21 07:50:44 +0100

    esigrpc: Add convenient helper function StringSliceToHeader

---
+++ esitag/backend/esigrpc/transform.go
@@ -0,0 +1,34 @@
+// Copyright 2016-2017, Cyrill @ Schumacher.fm and the CaddyESI Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package esigrpc
+
+import (
+	"net/http"
+
+	"github.com/corestoreio/errors"
+)
+
+// StringSliceToHeader converts the balanced slice "parts" into an http.Header.
+// It returns an error if the parts slice isn't balanced.
+func StringSliceToHeader(parts ...string) (http.Header, error) {
+	if len(parts)%2 == 1 {
+		return nil, errors.NewNotValidf("[esigrpc] Slice %v not balanced", parts)
+	}
+	h := http.Header{}
+	for i := 0; i < len(parts); i = i + 2 {
+		h.Add(parts[i], parts[i+1])
+	}
+	return h, nil
+}
---
+++ esitag/backend/esigrpc/transform_test.go
@@ -0,0 +1,39 @@
+// Copyright 2016-2017, Cyrill @ Schumacher.fm and the CaddyESI Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package esigrpc_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/SchumacherFM/caddyesi/esitag/backend/esigrpc"
+	"github.com/corestoreio/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringSliceToHeader(t *testing.T) {
+	t.Run("imbalanaced", func(t *testing.T) {
+		x := []string{"X-Content-Value"}
+		h, err := esigrpc.StringSliceToHeader(x...)
+		assert.Nil(t, h)
+		assert.True(t, errors.IsNotValid(err), "%+v", err)
+	})
+	t.Run("balanaced", func(t *testing.T) {
+		x := []string{"X-Content-Value", "a", "X-Content-Value", "b"}
+		h, err := esigrpc.StringSliceToHeader(x...)
+		assert.NoError(t, err, "%+v", err)
+		assert.Exactly(t, http.Header{"X-Content-Value": []string{"a", "b"}}, h)
+	})
+}
