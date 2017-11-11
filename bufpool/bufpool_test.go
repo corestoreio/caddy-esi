e4e1e2c195e1e46d8e9fc880cb9264b179c894be
Cyrill Schumacher <cyrill@schumacher.fm>
2017-11-11 13:35:37 +0100

    bufpool: Test imported incorrect buffer pool package ;-(

--- bufpool/bufpool_test.go
+++ bufpool/bufpool_test.go
@@ -3,12 +3,12 @@ package bufpool_test
 import (
 	"testing"
 
-	"github.com/corestoreio/csfw/util/bufferpool"
+	"github.com/corestoreio/caddy-esi/bufpool"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBufferPoolSize(t *testing.T) {
-	p := bufferpool.New(4096)
+	p := bufpool.New(4096)
 	assert.Exactly(t, 4096, p.Get().Cap())
 	assert.Exactly(t, 0, p.Get().Len())
 }
